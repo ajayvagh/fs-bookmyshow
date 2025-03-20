660693dff208e4dd1e0b1ec6b8c65efc87e3e4e6
ajayvagh <ajayvaghav8@gmail.com>
2025-03-20 14:46:14 +0530

    Routes for login and sign-up

--- server/index.js
+++ server/index.js
@@ -3,20 +3,21 @@ require('dotenv').config();
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
+const userRoutes = require("./routes/userRoutes")
 
 const dburl = process.env.MONGO_URI;
 
+app.use(express.json());
 
 mongoose.connect(dburl)
 .then(() => {
     console.log("Monog DB Connected .. ✅")
 })
 .catch((err) => {
-    console.log("Error Connecting MongoDB .. ❌", err.message)
+    console.log("Error Connecting MongoDB .. ❌", err);
 })
 
-
-
+app.use('/api/users', userRoutes);
 
 app.listen(8000, () => {
     console.log("Server Started .. ✅")
